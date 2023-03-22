4ca3e84a9c3c6aea1c227b38a1b2e4ae48d9ccfa
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-03-21 21:36:07 -0700

    front and back connected and displaying forecast data

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Weather extends React.Component {
+  render() {
+  return(
+    <>
+    {this.props.cityWeather.map((singleDay, idx) => {
+      
+      return(
+        <>
+        {/* key={idx} */}
+        <p>{singleDay.date}</p>
+        <p>{singleDay.description}</p>
+        </>
+      )
+    })}
+    </>
+  )
+
+  }
+}
+
+export default Weather;
