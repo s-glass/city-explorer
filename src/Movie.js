4ba15c54ac9af5a384fb3699e8b5c95733d5c34d
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-03-24 15:24:12 -0700

    card css

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 class Movie extends React.Component {
   render() {
-    return(
-      <>
-      <img src={`https://image.tmdb.org/t/p/w500/${this.props.movie.image}`} alt={this.props.movie.title}></img>
-      <p>{this.props.movie.title}</p>
-      <p>{this.props.movie.overview}</p>
+    return (
+      <>    
+              <Card shadow='0' border='primary' background='grey' className='cards' style={{ width: '18rem' }}>
+                <Card.Img src={`https://image.tmdb.org/t/p/w500/${this.props.movie.image}`} alt={this.props.movie.title}/>
+                <Card.Body className='text-primary'>
+                  <Card.Title> {this.props.movie.title}</Card.Title>
+                  <Card.Text>
+                    <p className='text-primary'>{this.props.movie.overview}</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
       </>
-    )
-  }
-}
+    )}};
 
 export default Movie;
